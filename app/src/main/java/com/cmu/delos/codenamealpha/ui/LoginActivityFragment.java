9dea9719dd1c6cf224120d631ec9faa04ad70ba0
Mrik Barkakati <Mrik@Mriks-MBP.sv.cmu.local>
2015-07-20 13:22:09 -0700

    added intentToSignUp

--- app/src/main/java/com/cmu/delos/codenamealpha/ui/LoginActivityFragment.java
+++ app/src/main/java/com/cmu/delos/codenamealpha/ui/LoginActivityFragment.java
@@ -1,16 +1,20 @@
 package com.cmu.delos.codenamealpha.ui;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.content.Intent;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class LoginActivityFragment extends Fragment {
+
+    private Button signUpBtn;
 
     public LoginActivityFragment() {
     }
@@ -18,6 +22,17 @@ public class LoginActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        signUpBtn = (Button)view.findViewById(R.id.signUpBtn);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.v("on click","signup Clicked");
+                Intent intentToSignUp = new Intent(getActivity(), SignUpActivity.class);
+                startActivity(intentToSignUp);
+            }
+        });
+        return view;
     }
 }
