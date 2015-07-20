e8f85bf1d3349fc0d8ab01cc079f6f227ebd5927
Mrik Barkakati <Mrik@Mriks-MBP.sv.cmu.local>
2015-07-20 16:42:02 -0700

    added search and order order

--- app/src/main/java/com/cmu/delos/codenamealpha/ui/ProfileActivity.java
+++ app/src/main/java/com/cmu/delos/codenamealpha/ui/ProfileActivity.java
@@ -1,12 +1,12 @@
 package com.cmu.delos.codenamealpha.ui;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class ProfileActivity extends ActionBarActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
