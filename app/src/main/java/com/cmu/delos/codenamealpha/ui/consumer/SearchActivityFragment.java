e0b9fdac4c1ecde915cebf3cca98110c0c8788de
Mrik Barkakati <Mrik@Mriks-MBP.sv.cmu.local>
2015-07-20 17:20:42 -0700

    intent for meal search

--- app/src/main/java/com/cmu/delos/codenamealpha/ui/consumer/SearchActivityFragment.java
+++ app/src/main/java/com/cmu/delos/codenamealpha/ui/consumer/SearchActivityFragment.java
@@ -1,10 +1,13 @@
 package com.cmu.delos.codenamealpha.ui.consumer;
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.cmu.delos.codenamealpha.ui.R;
 
@@ -13,12 +16,25 @@ import com.cmu.delos.codenamealpha.ui.R;
  */
 public class SearchActivityFragment extends Fragment {
 
+    private Button btnSearch;
+
     public SearchActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        btnSearch = (Button)view.findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.v("on click", "search Clicked");
+                Intent intentToSearch = new Intent(getActivity(), MealPageActivity.class);
+                startActivity(intentToSearch);
+            }
+        });
+        return view;
     }
 }
