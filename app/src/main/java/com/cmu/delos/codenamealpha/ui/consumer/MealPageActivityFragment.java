1610de2fa04d0774c6ded5013b8b3d898ff77b85
Mrik Barkakati <Mrik@Mriks-MBP.sv.cmu.local>
2015-07-20 17:56:16 -0700

    meal page

--- app/src/main/java/com/cmu/delos/codenamealpha/ui/consumer/MealPageActivityFragment.java
+++ app/src/main/java/com/cmu/delos/codenamealpha/ui/consumer/MealPageActivityFragment.java
@@ -1,10 +1,13 @@
 package com.cmu.delos.codenamealpha.ui.consumer;
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.cmu.delos.codenamealpha.ui.R;
 
@@ -13,12 +16,25 @@ import com.cmu.delos.codenamealpha.ui.R;
  */
 public class MealPageActivityFragment extends Fragment {
 
+    private ImageButton imageBtn1;
+
     public MealPageActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_meal_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_meal_page, container, false);
+        imageBtn1 = (ImageButton)view.findViewById(R.id.imageBtn1);
+        imageBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.v("on click", "meal Clicked");
+                Intent intentToViewMeal = new Intent(getActivity(), OrderMealActivity.class);
+                startActivity(intentToViewMeal);
+            }
+        });
+        return view;
     }
 }
