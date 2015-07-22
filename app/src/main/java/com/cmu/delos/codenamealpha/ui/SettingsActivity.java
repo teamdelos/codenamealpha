61e1157dd4e8c859bff0470dd044195898ed228e
famoruwa <famoruwa@berkeley.edu>
2015-07-21 17:00:51 -0700

    Updates: 1) Activity_Search 2) Fragment_Kitchen 3) Fragment_meal_offering_complete 4) Fragment_offer_meal.xml 5) Fragment_profile.xml

--- app/src/main/java/com/cmu/delos/codenamealpha/ui/SettingsActivity.java
+++ app/src/main/java/com/cmu/delos/codenamealpha/ui/SettingsActivity.java
@@ -1,16 +1,27 @@
 package com.cmu.delos.codenamealpha.ui;
 
+import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements SettingsActivityFragment.OnFragmentInteractionListener, ProfileActivityFragment.OnFragmentInteractionListener {
+
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+    /**
+     */
+    private CharSequence mTitle;
     DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +59,49 @@ public class SettingsActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.nav_header, container, false);
+            return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((ProfileActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
