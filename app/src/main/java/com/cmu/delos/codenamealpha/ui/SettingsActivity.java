7e6c028512ce58d25e18805c5d4c2107ef006451
Vignan Uppugandla <u.vignan@gmail.com>
2015-07-24 15:29:39 -0700

    subtle changes

--- app/src/main/java/com/cmu/delos/codenamealpha/ui/SettingsActivity.java
+++ app/src/main/java/com/cmu/delos/codenamealpha/ui/SettingsActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
