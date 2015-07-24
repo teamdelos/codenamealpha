76b68c3759efdc92fbc72f75b50fbba3d6b6b4ba
famoruwa <famoruwa@berkeley.edu>
2015-07-23 20:32:55 -0700

    Modified BuildOrder.java

--- app/src/main/java/com/cmu/delos/codenamealpha/Adapter/BuildOrder.java
+++ app/src/main/java/com/cmu/delos/codenamealpha/Adapter/BuildOrder.java
@@ -1,7 +1,9 @@
 package com.cmu.delos.codenamealpha.Adapter;
 
+import com.cmu.delos.codenamealpha.server.OrderServer;
+
 /**
  * Created by felixamoruwa on 7/22/15.
  */
-public abstract class BuildOrder extends ProxyOrder implements UpdateOrder, CreateOrder, FixOrder{
+public abstract class BuildOrder extends ProxyOrder implements UpdateOrder, CreateOrder, FixOrder, OrderServer{
 }
