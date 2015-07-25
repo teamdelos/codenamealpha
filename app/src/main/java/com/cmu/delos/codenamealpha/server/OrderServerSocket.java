5bfbfe4f354a7c27b21ec398fef223fffcead100
Mrik Barkakati <Mrik@Mriks-MBP.sv.cmu.local>
2015-07-24 17:00:10 -0700

    added login code and commented compile error classes

--- app/src/main/java/com/cmu/delos/codenamealpha/server/OrderServerSocket.java
+++ app/src/main/java/com/cmu/delos/codenamealpha/server/OrderServerSocket.java
@@ -1,7 +1,7 @@
-package com.cmu.delos.codenamealpha.server;
-
-/**
- * Created by felixamoruwa on 7/22/15.
- */
-public abstract class OrderServerSocket implements OrderServer {
-}
+//package com.cmu.delos.codenamealpha.server;
+//
+///**
+// * Created by felixamoruwa on 7/22/15.
+// */
+//public abstract class OrderServerSocket implements OrderServer {
+//}
